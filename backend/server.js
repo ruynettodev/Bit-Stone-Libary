0e3df7766dc97cf867b50939dd9de19a227fd9c3
Ruy <ruyn352@gmail.com>
2025-08-13 23:16:19 -0300

    fix: correcao de identacao

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const booksRoutes = require('./routes/books');
 
 const app = express();
-const PORT = process.env.PORT  3001;
+const PORT = process.env.PORT || 3001;
 
 // Middlewares de segurança e configuração
 app.use(helmet());
